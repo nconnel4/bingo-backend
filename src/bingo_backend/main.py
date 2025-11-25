677b94e4350cbd11273b7725a04f2c9d0faf2095
Nathan Connell <nconnel4@hotmail.com>
2025-11-25 10:42:39 -0500

    fix: add CORS policy

--- src/bingo_backend/main.py
+++ src/bingo_backend/main.py
@@ -1,6 +1,8 @@
 from functools import lru_cache
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from bingo_backend.config import Settings
 
 @lru_cache
@@ -16,5 +18,18 @@ from bingo_backend.space.models import *
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "https://polite-dune-04083480f.3.azurestaticapps.net/",
+    "https://bingo.basepointdesign.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
